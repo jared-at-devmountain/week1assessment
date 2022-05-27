6659b87c430e1601e6f300ac51befe28cd5f14f6
Jared Collier <jared@Jareds-MBP.localdomain>
2022-05-27 16:47:20 -0600

    password.js validator added

--- password.js
+++ password.js
@@ -1,3 +1,13 @@
+console.log('\n\n\n\n\n\n')
+
+console.log("       ^     ")
+console.log("      ^^^    ")
+console.log("     ^^^^^   ")
+console.log("    ^^^^^^^  ")
+console.log("      | |    ")
+console.log("MERRY CHRISTMAS")
+console.log('\n\n\n\n')
+
 console.log("Welcome to the password validator tool!")
 
 const readline = require('readline');
@@ -13,9 +23,25 @@ const reader = readline.createInterface({
 
 
 reader.question("Type the password that you want validated: \n", function(input){
-
-    console.log('the input was: ' + input)
 	
+    const errorArr = [];
+
+    if (input.length < 10){
+        errorArr.push('your password needs to be at least 10 characters long')
+    }
+    
+    if (!/\d/.test(input)) {
+        errorArr.push('your password needs a number')
+    }
+
+    if (errorArr.length === 0) {
+        console.log('Good password!')
+    } else {
+        console.log('Bad password because:')
+        for (let i =0; i < errorArr.length; i++) {
+            console.log(errorArr[i])
+        }
+    }
 
 
 	// This line closes the connection to the command line interface.
